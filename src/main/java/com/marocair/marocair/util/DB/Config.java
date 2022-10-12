834afd1b0b6d019bdd300e1c3ccf786ea1c3674a
mehdilagdimi <lagdimi.mehdi@gmail.com>
2022-10-12 11:07:02 +0100

    adding DB handler classes

---
+++ src/main/java/com/marocair/marocair/util/DB/Config.java
@@ -0,0 +1,24 @@
+package com.marocair.marocair.util.DB;
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class Config {
+        public static Dotenv dotenv = Dotenv.load();
+        private final static String URL = dotenv.get("URL") ;
+        private final static String USER = dotenv.get("USER");
+        //setup env variable for later use
+        private final static String PASSWORD = dotenv.get("PASSW");
+
+
+        public static String getUrl() {
+            return URL;
+        }
+
+        public static String getUser() {
+            return USER;
+        }
+
+        public static String getPassword() {
+            return PASSWORD;
+        }
+
+}
